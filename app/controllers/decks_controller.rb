18145348f5ff8beba176bc280f8a9c397095299c
Scott Southard <scottmsouthard@gmail.com>
2016-05-06 11:33:32 -0500

    Complete migrations

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,1 @@
+decks_controller.rb
---
+++ db/migrate/20160506111924_create_guesses.rb
@@ -0,0 +1,11 @@
+class CreateGuesses < ActiveRecord::Migration
+  def change
+    create_table :guesses do |t|
+      t.integer :card_id, null: false
+      t.boolean :first_try, null: false
+      t.integer :round_id, null: false
+      t.boolean :correct, null: false
+
+      t.timestamps
+  end
+end

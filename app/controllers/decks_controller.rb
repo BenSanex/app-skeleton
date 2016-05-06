f3e215daa019eb93efac13309edae340f57db5ec
Scott Southard <scottmsouthard@gmail.com>
2016-05-06 17:57:58 -0500

    fix logic!!!!!

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -13,24 +13,34 @@ get '/decks/:id' do
 end
 
 get '/decks/:deck_id/round' do
+  @cards = Card.where(deck_id: params[:deck_id])
   if session[:round_id] == nil
+    p "found nil round_id"
+    Guess.all.each {|guess| guess.destroy }
     @round = Round.create(user_id: session[:user_id], deck_id: params[:deck_id])
     session[:round_id] = @round.id
 
-    @cards = Card.where(deck_id: params[:deck_id])
-
-    @cards.each do |card|
-      Guess.create(card_id: card.id, first_try: true, round_id: @round.id, correct: false)
+  #   @cards.each do |card|
+  #     Guess.create(card_id: card.id, first_try: true, round_id: @round.id, correct: false)
+  #   end
+   end
+  @remaining_cards = []
+  @cards.each do |card|
+    if card.guesses.count > 0
+      if card.guesses.last.correct != true
+      @remaining_cards << card
+      end
+    else
+      @remaining_cards << card
     end
   end
 
-  @remaining_cards = Guess.where(correct: !true)
-
+  p @remaining_cards.count
   if @remaining_cards.length == 0
     redirect "/users/#{session[:user_id]}"
   else
     next_card = @remaining_cards.sample
-    redirect "/decks/#{params[:deck_id]}/cards/#{next_card.card_id}"
+    redirect "/decks/#{params[:deck_id]}/cards/#{next_card.id}"
   end
 end
 
@@ -42,12 +52,14 @@ end
 post '/decks/:deck_id/round' do
   @card = Card.find(params[:card_id])
   status = @card.verify_guess(params[:player_guess])
-  @guess = Guess.find_by(card_id: params[:card_id])
-  @guess.update_attributes(correct: status)
-  @guess.save
-  if status == false
-    @guess.update_attributes(first_try: false)
-    @guess.save
+  p status
+  if Guess.where(card_id: @card.id, round_id: session[:round_id]).count > 0
+    p 'found guess'
+    @guess = Guess.create(card_id: @card.id, first_try: false, round_id: session[:round_id], correct: status )
+  else
+    p "didn't find previous guess"
+   @guess = Guess.create(card_id: @card.id, first_try: true, round_id: session[:round_id], correct: status )
   end
+
   redirect "/decks/#{params[:deck_id]}/round"
 end

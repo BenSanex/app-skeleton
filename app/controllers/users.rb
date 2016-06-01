0dc6bc4b87d7ad51e2ce50c770d9390231a08059
Sean Eble & Tim Lantz <pair+sjeble.tlantz77@devbootcamp.com>
2016-06-01 18:19:05 -0500

    add working error messages

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,7 +2,7 @@ get '/users/signup' do
   erb :'users/signup'
 end
 
-post '/users' do
+post '/users/signup' do
   @user = User.new(params[:user])
   if @user.save
     redirect '/questions'
@@ -22,7 +22,7 @@ post '/users/login' do
     session[:user_id] = @user.id
     redirect '/questions'
   else
-    @errors = "Incorrect e-mail and/or password"
+    @errors = ["Incorrect e-mail and/or password"]
     erb :'users/login'
   end
 end

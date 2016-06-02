8838591c67700821fbd7341b91f0118edcb5a524
Kevin Fowle & Jennifer Cross <pair+kevin-fowle.nearlyjen@devbootcamp.com>
2016-06-02 17:03:58 -0500

    adds show styling and ajax comments

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,19 @@
-lines (23 sloc)  969 Bytes
-$(document).ready(function() {
 
-  $(".vote-form").on("submit", function(event) {
-    event.preventDefault();
+// $(document).ready(function() {
 
-    var form = $(this);
-    var url = form.attr("action");
-    var method = form.attr("method");
-    var data = form.serialize();
+//   $(".vote-form").on("submit", function(event) {
+//     event.preventDefault();
 
-    var request = $.ajax({url: url, method: method, data: data});
+//     var form = $(this);
+//     var url = form.attr("action");
+//     var method = form.attr("method");
+//     var data = form.serialize();
 
-    request.done(function(post) {
-      form.closest("article").find(".points").html(post.points);
-      // form.find(".upvote-button").css({color: "red"});
-    });
-  });
-});
+//     var request = $.ajax({url: url, method: method, data: data});
+
+//     request.done(function(post) {
+//       form.closest("article").find(".points").html(post.points);
+//       // form.find(".upvote-button").css({color: "red"});
+//     });
+//   });
+// });

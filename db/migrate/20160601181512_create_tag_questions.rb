90d52e5052d93a424906a1a1135d17c74d64069d
SJEble <sjeble@gmail.com>
2016-06-03 14:47:09 -0500

    answer nesting issue resolved

--- db/migrate/20160601181512_create_tag_questions.rb
+++ db/migrate/20160601181512_create_tag_questions.rb
@@ -1,8 +1,8 @@
 class CreateTagQuestions < ActiveRecord::Migration
   def change
     create_table :tag_questions do |t|
-      t.integer :tag_id, null: false
-      t.integer :question_id, null: false
+      t.integer :tag_id
+      t.integer :question_id
 
       t.timestamps null: false
     end
